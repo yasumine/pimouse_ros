ab748f719e464dc9c0d4835d82a6649fbe434ab2
yasumine <s206265@center.wakayama-u.ac.jp>
2019-05-10 10:04:33 +0000

    raspi1904

--- scripts/buzzer3.py
+++ scripts/buzzer3.py
@@ -5,15 +5,15 @@ from std_msgs.msg import UInt16
 def write_freq(hz=0):
   bfile = "/dev/rtbuzzer0"
   try:
-    with open(bfile,"w") as f:
-      f.write(str(hz) + "\n")
+      with open(bfile,"w") as f:
+          f.write(str(hz) + "\n")
   except IOError:
-    rospy.logerr("Can't write to " + bfile)
+      rospy.logerr("Can't write to " + bfile)
 
 def recv_buzzer(data):
-  write_freq(data.data)
+    write_freq(data.data)
 
 if __name__ == 'main':
-  rospy.init_node('buzzer')
-  rospy.Subscriber("buzzer",UInt16,recv_buzzer)
-  rospy.spin()
+    rospy.init_node('buzzer')
+    rospy.Subscriber("buzzer",UInt16,recv_buzzer)
+    rospy.spin()
