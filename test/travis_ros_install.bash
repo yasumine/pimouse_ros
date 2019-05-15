3fd1d32ee07ed22ce96db283cd65248cefced431
yasumine <40619813+yasumine@users.noreply.github.com>
2019-05-15 12:50:40 +0900

    Update travis_ros_install.bash

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,27 +1,26 @@
 #!/bin/bash -xve
 
-#add dummy files
-echo 1 2 3 4 | sudo tee /dev/rtlightsensor0
-echo 1 | sudo tee /dev/rtswitch{0,1,2}
-sudo touch /dev/rtmotor0 /dev/rtmotoren0 /dev/rtmotor_raw_{l,r}0 /dev/rtbuzzer0
-sudo chmod 666 /dev/rtmotor0 /dev/rtmotoren0 /dev/rtmotor_raw_{l,r}0 /dev/rtbuzzer0
-
 #required packages
+# In general, sudo should not be used for pip,
+# but I have written it for solving a path problem on Travis CI.
 sudo pip install catkin_pkg
 sudo pip install empy
 sudo pip install pyyaml
 sudo pip install rospkg
 
 #ros install
-git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
-cd ./ros_setup_scripts_Ubuntu14.04_server
+cd ..
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu16.04_server.git
+cd ./ros_setup_scripts_Ubuntu16.04_server
 bash ./step0.bash
 bash ./step1.bash
+
+sudo apt-get install ros-kinetic-tf ros-kinetic-tf2-geometry-msgs
 
 #catkin setup
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-source /opt/ros/indigo/setup.bash
+source /opt/ros/kinetic/setup.bash
 catkin_init_workspace
 cd ~/catkin_ws
 catkin_make
